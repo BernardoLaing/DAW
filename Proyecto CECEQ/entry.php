904e27c787546215cdcf392d74e247932aaac0f5
Bernardo Laing <Bernardo.laing@gmail.com>
2018-02-11 15:27:20 -0600

    Ajustes a entry.php

--- Proyecto CECEQ/entry.php
+++ Proyecto CECEQ/entry.php
@@ -1,6 +1,7 @@
 <?php include("partials/_header.html"); ?>
 <?php include("partials/_top_bar.html"); ?>
-<div class="container">
+<br /><br />
+<div class="container shadow">
     <div class="row">
         <div class="col-sm-12">
                 <div class="text-center">
@@ -14,7 +15,7 @@
     <div class="row">
 
         <div class="col-sm-12">
-            <div class='form-group row'>
+            <div class='form-group'>
                 <label for="user_number">Número de Usuario</label>
                 <div class="col-md-10">
                     <input class="form-control" id="user_number" name="user[number]" size="30" type="text" />
@@ -25,7 +26,7 @@
         <div class='row'>
 
             <div class='col-sm-4'>    
-                <div class='form-group row'>
+                <div class='form-group'>
                     <label for="user_name">Nombre(s).</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_name" name="user[name]" size="30" type="text" />
@@ -34,7 +35,7 @@
             </div>
 
             <div class='col-sm-4'>
-                <div class='form-group row'>
+                <div class='form-group'>
                     <label for="user_paternal">Apellido P.</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_paternal" name="user[paternal]" required="true" size="30" type="text" />
@@ -43,7 +44,7 @@
             </div>
 
             <div class='col-sm-4'>
-                <div class='form-group row '>
+                <div class='form-group'>
                     <label for="user_maternal">Apellido M.</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_maternal" name="user[maternal]" required="true" size="30" type="text" />
@@ -56,7 +57,7 @@
         <div class='row'>
 
             <div class='col-sm-4'>
-                <div class='form-group row'>
+                <div class='form-group'>
                     <label for="user_age">Edad.</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_age" name="user[age]" required="true" size="30" type="text" />
@@ -65,7 +66,7 @@
             </div>
 
             <div class='col-sm-4'>
-                <div class='form-group row'>
+                <div class='form-group'>
                     <label for="user_grade">Grado de Estudio.</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_grade" name="user[grade]" required="true" size="30" type="text" />
@@ -74,7 +75,7 @@
             </div>
 
             <div class='col-sm-4'>
-                <div class='form-group row'>
+                <div class='form-group'>
                     <label for="user_ocupation">Ocupación.</label>
                     <div class="col-md-12">
                         <input class="form-control" id="user_ocupation" name="user[ocupation]" required="true" size="30" type="text" />
@@ -87,7 +88,7 @@
         <div class="row">
 
             <div class="col-sm-6">
-                <div class="form-group row">
+                <div class="form-group">
                     <div class="col-md-12">
                     <a class="btn btn-outline-secondary mx-auto" href="menu.php"><img src="src/btns/iconReturn.png" alt="Regresar" width="50" height="50"></a>
     <!--                    <button type="submit" class="btn btn-outline-secondary mx-auto">Iniciar Sesión</button>-->
@@ -96,7 +97,7 @@
             </div>
 
             <div class="col-sm-3">
-                <div class="form-group row">
+                <div class="form-group">
                     <div class="col-md-12">
                     <a class="btn btn-outline-secondary mx-auto" href="#">Buscar Visitante</a>
     <!--                    <button type="submit" class="btn btn-outline-secondary mx-auto">Iniciar Sesión</button>-->
@@ -105,7 +106,7 @@
             </div>
 
             <div class="col-sm-3">
-                <div class="form-group row">
+                <div class="form-group">
                     <div class="col-md-12">
                         <button type="submit" class="btn btn-outline-secondary mx-auto">Registrar entrada</button>
                     </div>
