4121a340b39ebad7fc3a3d0567708acb1ef9a467
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-02-14 15:59:41 -0600

    inicio de lend_return.php interfaz

---
+++ Proyecto CECEQ/lend_return.php
@@ -0,0 +1,56 @@
+<?php include("partials/_header.html"); ?> <!--Acaba en la primera etiqueta body-->
+<?php include("partials/_top_bar.html");?>  <!--Es la etiqueta Nav-->
+<br><br>
+<div class="container shadow">
+  <!--Titulo de intent-->
+  <div class="container">
+    <div class="row text-center">
+        <div class="col-sm-12 mx-auto">
+          <br>
+            <div class="display-3">Préstamo & Devolución</div>
+        </div>
+    </div>
+  </div>
+  <!--ID USUARIO--> <br>
+  <div class="row">
+    <div class="col-sm-4"></div> <!--NADA-->
+    <div class='col-sm-4'>
+        <div class='form-group'>
+            <label for="user_ocupation">Id. Credencial</label>
+            <input class="form-control" id="user_ocupation" name="user[ocupation]" required="false" size="30" type="text" />
+        </div>
+    </div>
+    <div class="col-sm-4"></div> <!--NADA-->
+  </div>
+  <!--ID LIBROS-->
+  <div class="row">
+      <div class="col-sm-1"></div> <!--NADA-->
+      <div class='col-sm-2'>
+          <div class='form-group'>
+              <label for="user_ocupation">Código de Libro</label>
+              <input class="form-control" id="user_ocupation" name="user[ocupation]" required="false" size="30" type="text" />
+          </div>
+      </div>
+      <div class="col-sm-2"></div> <!--NADA-->
+      <div class='col-sm-2'>
+          <div class='form-group'>
+              <label for="user_ocupation">Código de Libro</label>
+              <input class="form-control" id="user_ocupation" name="user[ocupation]" required="false" size="30" type="text" />
+          </div>
+      </div>
+      <div class="col-sm-2"></div> <!--NADA-->
+      <div class='col-sm-2'>
+          <div class='form-group'>
+              <label for="user_ocupation">Código de Libro</label>
+              <input class="form-control" id="user_ocupation" name="user[ocupation]" required="false" size="30" type="text" />
+          </div>
+      </div>
+      <div class="col-sm-1"></div> <!--NADA-->
+  </div>
+  <!--BOTONES-->
+
+</div>
+
+
+
+<?php include("partials/_footer.html"); ?> <!--Empieza en los scroipts-->
