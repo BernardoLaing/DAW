21594f78c24ba5c8838d9b841f8c7a59ef16045b
unknown <a01172309@itesm.mx>
2018-02-15 19:33:13 -0600

    Agregar botón clasificaciones a interfaz catalog.php

--- Proyecto CECEQ/catalog.php
+++ Proyecto CECEQ/catalog.php
@@ -31,9 +31,100 @@
 
             <div class='col-sm-4'>
                 <div class='form-group'>
-                    <label for="user_maternal">Clasificación</label>
+                    <label for="user_maternal">Clasificaciones</label>
                     <div class="col-md-12">
-                        <input class="form-control" id="user_maternal" name="user[maternal]" required="true" size="30" type="text" placeholder="80"/>
+                        <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#clasif">Seleccionar...</button>
+                        <div class="modal fade" id="clasif" tabindex="-1" role="dialog" aria-labelledby="clasif" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="clasifTitle">Clasificaciones</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="form-check">
+
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        000 Generalidades. <br/><br/>
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        100 Filosofía y psicología.
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        200 Religión.
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        300 Ciencias sociales. <br/><br/>
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        400 Lenguas.
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        500. Ciencias naturales y matemáticas.
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        600 Tecnología. <br/><br/><br/><br/>
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        700 Las artes Bellas artes y artes decorativas.
+                                                    </label>
+                                                </div> 
+                                                <div class="col-sm-4">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        800 Literatura y retórica.
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                            <div class="col-sm-12 text-center">
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input" id="generalidades" value="000">
+                                                        900 Geografía e historia.
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-outline-secondary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -159,4 +250,5 @@
         </div>
     </form>
 </div>
+
 <?php include("partials/_footer.html"); ?>
