0fb2c45ba4f71693e42ecc8e9f5d9f8f21f377ef
MartinAntonio123 <a01701167@itesm.mx>
2018-02-15 19:16:03 -0600

    actividades completas

---
+++ Proyecto CECEQ/prueba.php
@@ -0,0 +1,9 @@
+<?php include("partials/_header.html"); ?>
+<?php include("partials/_top_bar.html"); ?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">Total cost</div>
+    <div class="col-md-6"><span class="pull-right">$42</span></div>
+  </div>
+</div>
+<?php include("partials/_footer.html"); ?>
