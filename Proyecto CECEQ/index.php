7b3ff84fde5b2c8038d88664c598b4a70cbed45e
Bernardo Laing <bernardo.laing@gmail.com>
2018-02-21 09:14:54 -0600

    Begin login form validation

--- Proyecto CECEQ/index.php
+++ Proyecto CECEQ/index.php
@@ -1,4 +1,31 @@
 <?php include("partials/_header.html"); ?>
+
+<?php
+$userError = "";
+$pswdError = "";
+if(isset($_POST["submit"])){
+    if(empty($_POST["user"])){
+        $userError = "Ingresa tu usuario";
+    }else {
+        $user = process($_POST["user"]);
+        if(!preg_match("/^[a-zA-Z0-9_]{4,}$/", $user)){
+            $userError = "El usuario es invalido";
+        }
+    }
+}
+
+function process($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+?>
+
 <?php include("partials/_second_top_bar.html"); ?>
 <?php include("login_form.html"); ?>
+
+
 <?php include("partials/_footer.html"); ?>
