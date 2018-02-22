ebf8a3a1fabea75d4a581bc6d1761273300c5e69
Bernardo Laing <Bernardo.laing@gmail.com>
2018-02-22 09:14:41 -0600

    Modificaciones a rols.php

--- Proyecto CECEQ/rols.php
+++ Proyecto CECEQ/rols.php
@@ -1,108 +1,100 @@
 <?php include("partials/_header.html"); ?>
 <?php include("partials/_top_bar.html"); ?>
 <br /><br />
-<div class="container mdiv shadow text-center">
+<div class="container shadow">
     <div class="mx-auto text-center"> <!-- mx-auto da un padding y border automatico-->
         <div class="display-3">Roles</div> <!--display son headdings mas grandes-->
     </div>
     <br>
     <br>
     <div class="row text-center">
-      <div class=" col-sm-12 col-md-1 "></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-        <div class="card-body">
-          <p class="card-title"><b>Rol 6</b></p>
-          <div class="card-text">
-            Esta es la descripcion del rol mas cool de ceceq
-          </div> <br>
-          <div class='projects-button btn btn-secondary'>Ver permisos</div>
-          <div class='projects'> <br>
-            <p >Privilegio 1</p>
-            <p>Privilegio 2</p>
-            <p>Privilegio 3</p>
-          </div>
-        </div>
-      </div>
-      <div class=" col-sm-12 col-md-2"></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-          <div class="card-body">
-            <p class="card-title"><b>Rol 6</b></p>
-            <div class="card-text">
-              Esta es la descripcion del rol mas cool de ceceq
-            </div> <br>
-            <div class='projects-button btn btn-secondary'>Ver permisos</div>
-            <div class='projects'> <br>
-              <p >Privilegio 1</p>
-              <p>Privilegio 2</p>
-              <p>Privilegio 3</p>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
             </div>
-          </div>
         </div>
-      <div class=" col-sm-12 col-md-2"></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-          <div class="card-body">
-            <p class="card-title"><b>Rol 6</b></p>
-            <div class="card-text">
-              Esta es la descripcion del rol mas cool de ceceq
-            </div> <br>
-            <div class='projects-button btn btn-secondary'>Ver permisos</div>
-            <div class='projects'> <br>
-              <p >Privilegio 1</p>
-              <p>Privilegio 2</p>
-              <p>Privilegio 3</p>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
             </div>
-          </div>
         </div>
-      <div class=" col-sm-12 col-md-1"></div>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
+            </div>
+        </div>
     </div>
     <br><br>
     <div class="row text-center">
-      <div class=" col-sm-12 col-md-1 "></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-        <div class="card-body">
-          <p class="card-title"><b>Rol 6</b></p>
-          <div class="card-text">
-            Esta es la descripcion del rol mas cool de ceceq
-          </div> <br>
-          <div class='projects-button btn btn-secondary'>Ver permisos</div>
-          <div class='projects'> <br>
-            <p >Privilegio 1</p>
-            <p>Privilegio 2</p>
-            <p>Privilegio 3</p>
-          </div>
-        </div>
-      </div>
-      <div class=" col-sm-12 col-md-2"></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-          <div class="card-body">
-            <p class="card-title"><b>Rol 6</b></p>
-            <div class="card-text">
-              Esta es la descripcion del rol mas cool de ceceq
-            </div> <br>
-            <div class='projects-button btn btn-secondary'>Ver permisos</div>
-            <div class='projects'> <br>
-              <p >Privilegio 1</p>
-              <p>Privilegio 2</p>
-              <p>Privilegio 3</p>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
             </div>
-          </div>
         </div>
-      <div class=" col-sm-12 col-md-2"></div>
-      <div class="col-sm-12 col-md-2 card shadow">
-          <div class="card-body">
-            <p class="card-title"><b>Rol 6</b></p>
-            <div class="card-text">
-              Esta es la descripcion del rol mas cool de ceceq
-            </div> <br>
-            <div class='projects-button btn btn-secondary'>Ver permisos</div>
-            <div class='projects'> <br>
-              <p >Privilegio 1</p>
-              <p>Privilegio 2</p>
-              <p>Privilegio 3</p>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
             </div>
-          </div>
         </div>
-      <div class=" col-sm-12 col-md-1"></div>
+        <div class="col-sm-12 col-md-4">
+            <div class="card-body shadow">
+                <p class="card-title"><b>Rol 1</b></p>
+                <div class="card-text">
+                    Esta es la descripcion del rol mas cool de ceceq
+                </div> <br>
+                <div class='projects-button btn btn-secondary'>Ver permisos</div>
+                <div class='projects'> <br>
+                    <p >Privilegio 1</p>
+                    <p>Privilegio 2</p>
+                    <p>Privilegio 3</p>
+                </div>
+            </div>
+        </div>
     </div>
     <br />
     <div class="row">
