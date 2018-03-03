13691189d4439c99e44ab29bdc9b8ad923822950
Bernardo Laing <Bernardo.laing@gmail.com>
2018-03-02 23:20:30 -0600

    Roles se cargan de DB

--- Proyecto CECEQ/js/script.js
+++ Proyecto CECEQ/js/script.js
@@ -87,7 +87,7 @@ function showPreview(objFileInput) {
         var fileReader = new FileReader();
         fileReader.onload = function (e) {
             $("#targetLayer").html('<img src="'+e.target.result+'" width="200px" height="200px" class="upload-preview" />');
-			$("#targetLayer").css('opacity','0.7');
+//			$("#targetLayer").css('opacity','0.7');
 			$(".icon-choose-image").css('opacity','0.5');
         }
 		fileReader.readAsDataURL(objFileInput.files[0]);
