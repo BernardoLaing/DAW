5e2a659d4294d11a69fe80726ceac31152994005
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-03-14 21:03:44 -0600

    insertar prestamo terminado

--- Proyecto CECEQ/lend_return.php
+++ Proyecto CECEQ/lend_return.php
@@ -11,6 +11,7 @@
   $idLibroDos =  isset($_POST["user"]["libroDos"]) && is_numeric($_POST["user"]["libroDos"]);
   $idLibroTres = isset($_POST["user"]["libroTres"]) && is_numeric($_POST["user"]["libroTres"]);
   $ingresoLibro = false;
+  $Prestamo = 0;
 
   if($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_POST["prestamo"]) || isset($_POST["devolucion"]))){
       echo "PASO 1";
@@ -19,16 +20,26 @@
     }
     if($idCredencial && $ingresoLibro && isset($_POST["prestamo"])){
       echo "PRESIONO PRESTAMO";
+        $Prestamo = 1;
         $aux_librouno = $_POST["user"]["libroUno"];
         $aux_credencial = $_POST["user"]["credencial"];
         $_SESSION['ok'] = true;
         $_SESSION['tipo'] = 'Préstamo';
         $_SESSION['libro'] = $aux_librouno;
         $_SESSION['credencial'] = $aux_credencial;
-    //    $hoy = 'hoy'; //getdate()'';
+      
+        $date = new DateTime();
+        $dateLend = $date->format('Y-m-d H:i:s');
+
+
+        $date2 = new DateTime();
+        $date2 = $date2->modify('+7 day');
+        $dateReturn = $date2->format('Y-m-d H:i:s');
+
     //    $hoyMasSiete = 'una Semana mas'; //strtotime("+7 Days");
   }else if($idCredencial && $ingresoLibro && isset($_POST["devolucion"])){
     echo "PRESIONO RETORNO";
+    $Prestamo = 2;
     $aux_librouno = $_POST["user"]["libroUno"];
     $aux_credencial = $_POST["user"]["credencial"];
     $_SESSION['ok'] = true;
@@ -43,14 +54,24 @@
 
   }
 
-  if(isset($_POST["aceptar"])){
-    echo 'ACEPTO';
-     Registrar_Prestamo_Devolucion($_SESSION["credencial"], $_SESSION["libro"]);
+  if(isset($_POST["aceptar"]) && ($Prestamo == 1)){ //El cuenta habiente esta ACEPTANDO un PRESTAMO
+    echo 'ACEPTO PRESTAMO ';
+     insertLend($_SESSION["libro"], $_SESSION["credencial"], $_SESSION['hoy'], $_SESSION['hoy2']);
      $_SESSION["credencial"] = null;
      $_SESSION["libro"] = null;
-  }else if(isset($_POST["cancelar"])){
-    echo "CANCELAR";
+  }else if(isset($_POST["aceptar"]) && ($Prestamo == 2)){//El cuenta habiente esta acepACEPTANDOtando una DEVOLUCION
+    echo 'ACEPTO DEVOLUCION';
+     insertLend($_SESSION["libro"], $_SESSION["credencial"], $_SESSION['hoy'], $_SESSION['hoy2']);
+     $_SESSION["credencial"] = null;
+     $_SESSION["libro"] = null;
+  }else if(isset($_POST["cancelar"]) && ($Prestamo == 1)){ //El cuenta habiente esta CANCELANDO un PRESTAMO
+    echo "CANCELAR PRESTAMO";
+    $_SESSION["credencial"] = null;
+    $_SESSION["libro"] = null;
+  }else if(isset($_POST["cancelar"]) && ($Prestamo == 2)){ //El cuenta habiente esta CANCELANDO una DEVOLUCION
+    echo "CANCELAR DEVOLUCION";
     $_SESSION["credencial"] = null;
     $_SESSION["libro"] = null;
   }
+  ($Prestamo == 0);
  ?>
