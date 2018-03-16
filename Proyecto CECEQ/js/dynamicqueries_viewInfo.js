2615ac5338ce3df96d1c79668194e39b93d2483b
EduardoCuestaC <A00570171@itesm.mx>
2018-03-15 23:08:28 -0600

    habilitar ajax en selects para dinamicqueries_viewInfo

--- Proyecto CECEQ/js/dynamicqueries_viewInfo.js
+++ Proyecto CECEQ/js/dynamicqueries_viewInfo.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("input").each(function(){
         $(this).keyup(function(){
             $.post("controller/entrySearch_controller.php",{
-               user : 
+               user :
                 {
                     number : $("#user_number").val(),
                     name : $("#user_name").val(),
@@ -17,6 +17,27 @@ $(document).ready(function(){
                 $("#user_table").html(data);
                 readyForDisplay();
             });
+        });
+    });
+
+    $("select").each(function(){
+        $(this).change(function(){
+            $.post("controller/entrySearch_controller.php",{
+                    user :
+                        {
+                            number : $("#user_number").val(),
+                            name : $("#user_name").val(),
+                            paternal : $("#user_paternal").val(),
+                            maternal : $("#user_maternal").val(),
+                            birthday : $("#user_birthday").val(),
+                            user_grade : $("#user_grade").val(),
+                            gender : $("#user_gender").val()
+                        }
+                },
+                function(data,status){
+                    $("#user_table").html(data);
+                    readyForDisplay();
+                });
         });
     });
 
