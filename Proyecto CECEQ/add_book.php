151e853998b5aeb7d7b20eaa002e91bda3ef8e2b
MartinAntonio123 <a01701167@itesm.mx>
2018-03-20 20:22:15 -0600

    agregar mensaje de success en insertar libro

--- Proyecto CECEQ/add_book.php
+++ Proyecto CECEQ/add_book.php
@@ -11,16 +11,17 @@ if(!empty($_POST))
     $idAutor;
     $idTitulo;
     $idEjemplar;
+    $retro = '';
     $results = buscarAutorN($_POST["book"]["author"], $_POST["book"]["apaterno"], $_POST["book"]["amaterno"]); //insetrar autor
     if(mysqli_num_rows($results) == 0) 
     {
         insertAutor($_POST["book"]["author"], $_POST["book"]["apaterno"], $_POST["book"]["amaterno"]);//insetrar autor
-        echo 'autor insertado';  //retroalimentacion
+        $retro = $retro.'autor insertado';  //retroalimentacion
     }
     
-    echo $_POST["book"]["author"]; //retroalimentacion
-    echo $_POST["book"]["apaterno"];//retroalimentacion
-    echo $_POST["book"]["amaterno"];//retroalimentacion
+    $retro = $retro. ' '. $_POST["book"]["author"]; //retroalimentacion
+    $retro = $retro. ' '. $_POST["book"]["apaterno"];//retroalimentacion
+    $retro = $retro. ' '. $_POST["book"]["amaterno"];//retroalimentacion
     
     $results = buscarAutorN($_POST["book"]["author"], $_POST["book"]["apaterno"], $_POST["book"]["amaterno"]); //insetrar autor
     if($row = mysqli_fetch_assoc($results))
@@ -28,16 +29,16 @@ if(!empty($_POST))
         $idAutor=$row['idAutor'];
     }
     
-    echo ' id= ' .$idAutor. '<br>';//retroalimentacion
+    $retro = $retro. ' id autor= ' .$idAutor. ' ';//retroalimentacion
     
     $results = buscarTitulo($_POST["book"]["title"], $_POST["book"]["year"]); //insetrar titulo
     if(mysqli_num_rows($results) == 0)
     {
         insertTitulo($_POST["book"]["title"], $_POST["book"]["year"]);//insetrar titulo
-        echo 'titulo insertado';//retroalimentacion
+        $retro = $retro. 'titulo insertado';//retroalimentacion
     }
     
-    echo $_POST["book"]["title"]. ' ' .$_POST["book"]["year"]; //retroalimentacion
+    $retro = $retro.' '. $_POST["book"]["title"]. ' ' .$_POST["book"]["year"]; //retroalimentacion
     
     $results = buscarTitulo($_POST["book"]["title"], $_POST["book"]["year"]); //insetrar titulo
     if($row = mysqli_fetch_assoc($results))
@@ -45,26 +46,26 @@ if(!empty($_POST))
         $idTitulo=$row['idTitulo'];
     }
     
-    echo ' id= ' .$idTitulo. '<br>';//retroalimentacion
+    $retro = $retro.' id= ' .$idTitulo;//retroalimentacion
     
     $results = buscarAutorTitulo($idTitulo, $idAutor); //crear relacion autor y titulo
     if(mysqli_num_rows($results) == 0)
     {
         insertAutorTitulo($idAutor, $idTitulo);
-        echo 'relacion titulo autor insertado <br>'; //retroalimentacion
+        //echo 'relacion titulo autor insertado <br>'; //retroalimentacion
     }
     
-    echo 'id titulo '.$idTitulo. ' idAutor ' . $idAutor. '<br>'; //retroalimentacion
+    //echo 'id titulo '.$idTitulo. ' idAutor ' . $idAutor. '<br>'; //retroalimentacion
     
 
     insertEjemplar($_POST["book"]["isbn"], $_POST["book"]["shelf"], $_POST["book"]["editorial"], $_POST["book"]["yeare"], $_POST["book"]["vol"], $idTitulo, $_POST["book"]["colection"], $_POST["book"]["edicion"], $_SESSION["user"] );//insetrar ejemplar
-    echo 'ejemplar insertado ';//retroalimentacion
-    echo 'isbn '.$_POST["book"]["isbn"].' estante '. $_POST["book"]["shelf"].' editorial '.$_POST["book"]["editorial"]. ' anio ' .$_POST["book"]["yeare"]. 'vol'. $_POST["book"]["vol"].' titulo' .$idTitulo; //retroalimentacion
+    $retro = $retro. 'ejemplar insertado ';//retroalimentacion
+    //echo 'isbn '.$_POST["book"]["isbn"].' estante '. $_POST["book"]["shelf"].' editorial '.$_POST["book"]["editorial"]. ' anio ' .$_POST["book"]["yeare"]. 'vol'. $_POST["book"]["vol"].' titulo' .$idTitulo; //retroalimentacion
     insertCategoriaTitulo($idTitulo, $_POST["clasificacion"]);
-    echo '<br> clasificacion: '.$_POST["clasificacion"]; //retroalimentacion
+    //echo '<br> clasificacion: '.$_POST["clasificacion"]; //retroalimentacion
     $idEjemplar = lastIndexEjemplar();
     insertEjemplarEstado($idEjemplar, 5);
-
+    echo '<script> alert("'. $retro . '")</script>';
    
 }
 ?>
