727e1ffab6f63c39882b5c39b10a1e2707fd4ac6
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-04-11 17:46:34 -0500

    Modales en toda la seccion de Control de Acceso

--- Proyecto CECEQ/deleteAccount.php
+++ Proyecto CECEQ/deleteAccount.php
@@ -3,7 +3,7 @@ session_start();
 require_once('model/RBAC-utils.php');
 
 if(deleteUser($_GET["user"])){
-    $_SESSION['success_msg'] = "La cuenta fue eliminada con éxito";
+   // $_SESSION['success_msg'] = "La cuenta fue eliminada con éxito";
 }else{
     $_SESSION['success_msg'] = "La cuenta no pudo ser eliminada";
 }
