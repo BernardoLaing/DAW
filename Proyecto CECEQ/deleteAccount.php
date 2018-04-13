3887ed8db9a46def285ba8c8b46b38fc9d1d2171
Bernardo Laing <Bernardo.laing@gmail.com>
2018-04-13 13:21:13 -0500

    Pruebas de editar y eliminar cuentas

--- Proyecto CECEQ/deleteAccount.php
+++ Proyecto CECEQ/deleteAccount.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
 require_once('model/RBAC-utils.php');
-
-if(deleteUser($_GET["user"])){
-   // $_SESSION['success_msg'] = "La cuenta fue eliminada con éxito";
+if($_SESSION['user'] == $_GET['user']){
+    $_SESSION['success_msg'] = "No puedes eliminar tu propia cuenta";
 }else{
-    $_SESSION['success_msg'] = "La cuenta no pudo ser eliminada";
+    if(deleteUser($_GET["user"])){
+       // $_SESSION['success_msg'] = "La cuenta fue eliminada con éxito";
+    }else{
+        $_SESSION['success_msg'] = "La cuenta no pudo ser eliminada";
+    }
 }
 header('Location:cuentas.php');
 ?>
