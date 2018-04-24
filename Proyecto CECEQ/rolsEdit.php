d0098e1a8ed01d45678582422fe3191a76adfeec
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-04-24 15:33:39 -0500

    Modal en edit rol

--- Proyecto CECEQ/rolsEdit.php
+++ Proyecto CECEQ/rolsEdit.php
@@ -10,6 +10,18 @@ $permissions = getTable('operacion');
 $rol = getRol($_GET['idRol']);
 ?>
 <?php 
+//echo $_SESSION['error_type'];
+//echo "dfghjkl";
 include("html/rolsEdit.html");
+include("modals/modal_error.html");
+if(isset($_SESSION['error_type']) && $_SESSION['error_type'] === "rolEditConflict"){
+    echo "<script>
+            $(document).ready(function (e) {
+                $('#myModal').modal('show');
+            });
+        </script>";
+    $_SESSION['error_type'] = null;
+    $_SESSION['error_msg'] = null;
+}
 include("partials/_footer.html"); 
 ?>
